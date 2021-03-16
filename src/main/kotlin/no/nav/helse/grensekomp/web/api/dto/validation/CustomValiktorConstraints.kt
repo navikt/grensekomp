42510cc85c8bf57ac6c043c6fd79d5ba8cd774d3
tuxbear <ole-andre.riga.johansen@nav.no>
2021-03-16 15:17:41 +0100

    Fikser validinger

--- src/main/kotlin/no/nav/helse/grensekomp/web/api/dto/validation/CustomValiktorConstraints.kt
+++ src/main/kotlin/no/nav/helse/grensekomp/web/api/dto/validation/CustomValiktorConstraints.kt
@@ -32,20 +32,12 @@ class RefusjonsdagerKanIkkeOverstigePeriodelengdenConstraint : CustomConstraint
 
 fun <E> Validator<E>.Property<Int?>.refusjonsDagerIkkeOverstigerPeriodelengder(fom: LocalDate, tom: LocalDate) =
     this.validate(RefusjonsdagerKanIkkeOverstigePeriodelengdenConstraint()) { antallDagerMedRefusjon ->
-        antallDagerMedRefusjon != null && ChronoUnit.DAYS.between(fom, tom.plusDays(1)) < antallDagerMedRefusjon
+        !(antallDagerMedRefusjon != null && ChronoUnit.DAYS.between(fom, tom.plusDays(1)) < antallDagerMedRefusjon)
     }
 
 class TomPeriodeKanIkkeHaBeloepConstraint : CustomConstraint
 
 fun <E> Validator<E>.Property<Double?>.tomPeriodeKanIkkeHaBeloepConstraint(antallDagerMedRefusjon: Int) =
     this.validate(TomPeriodeKanIkkeHaBeloepConstraint()) { beloep ->
-        beloep != null && antallDagerMedRefusjon == 0 && beloep > 0
-    }
-
-class RefusjonsdagerInnenforGyldigPeriodeConstraint : CustomConstraint
-
-fun <E> Validator<E>.Property<Periode?>.refusjonsdagerInnenforGyldigPeriode(refusjonsdagerFom: LocalDate) =
-    this.validate(RefusjonsdagerInnenforGyldigPeriodeConstraint()) { p ->
-        val validDays = ChronoUnit.DAYS.between(refusjonsdagerFom, p?.tom?.plusDays(1))
-        p != null && (p.fom >= refusjonsdagerFom || (p.antallDagerMedRefusjon == 0 || p.antallDagerMedRefusjon <= validDays))
+        !(beloep != null && antallDagerMedRefusjon == 0 && beloep > 0)
     }
