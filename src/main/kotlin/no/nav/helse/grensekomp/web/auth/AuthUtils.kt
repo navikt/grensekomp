e935f17f834b10263ab6a7030303bd2e2adffce7
Gustav Berggren <gustav.berggren@nav.no>
2022-03-08 11:51:47 +0100

    Idporten token pid

--- src/main/kotlin/no/nav/helse/grensekomp/web/auth/AuthUtils.kt
+++ src/main/kotlin/no/nav/helse/grensekomp/web/auth/AuthUtils.kt
@@ -22,7 +22,8 @@ fun PipelineContext<Unit, ApplicationCall>.authorize(authorizer: AltinnAuthorize
 @KtorExperimentalAPI
 fun hentIdentitetsnummerFraLoginToken(config: ApplicationConfig, request: ApplicationRequest): String {
     val tokenString = getTokenString(config, request)
-    return JwtToken(tokenString).subject
+    val pid = JwtToken(tokenString).jwtTokenClaims.get("pid")
+    return pid?.toString() ?: JwtToken(tokenString).subject
 }
 
 @KtorExperimentalAPI
